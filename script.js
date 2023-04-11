ce920911f922b4dc2a3b2de50de32962b239bad7
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-11 23:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+if(sampleObject==key)	{
+	return true;
+}else{
+	return false;
+}
 }
 
 // Do not change the code below
